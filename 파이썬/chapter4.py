684c2521603f058e1787d8919757a04a3f7ecce7
sfeedd <qowjdgus86@naver.com>
2024-09-05 18:42:57 +0900

    intial commit

---
+++ 파이썬/chapter4.py
@@ -0,0 +1,24 @@
+# 더하기
+print(3 + 2)
+
+# 빼기
+print(3 - 2)
+
+# 곱하기
+print(3 * 2)
+
+# /
+print(7 / 2)
+
+# //
+print(7 // 2)
+
+# %
+print(7 % 2)
+
+# divmod
+print(divmod(3, 2))
+print(divmod(7, 4))
+print(divmod(13, 2))
+print(divmod(19, 4))
+print(divmod(23, 7))
